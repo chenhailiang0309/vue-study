7cd7f2ca00e93bcd3ee92f481d23cb1a1ed8ffc2
chenhailiang <958304849@qq.com>
2020-03-24 18:44:49 +0800

    0324

--- 源码学习/code-xiaomage/MVue.js
+++ 源码学习/code-xiaomage/MVue.js
@@ -1,3 +1,32 @@
+const compileUtil = {
+    getVal(expr, vm) {
+        console.log(vm.$data)
+        return expr.split('.').reduce((data,currentVal)=>{
+            console.log(currentVal)
+            return data[currentVal]
+        },vm.$data)
+    },
+    text(node, expr, vm) {
+        const value = this.getVal(expr, vm);// expr:text   <div v-text="person.text"></div>
+        // const value = vm.$data[expr]
+        this.updater.textUpdater(node, value)
+    },
+    html(node, expr, vm) {
+
+    },
+    model(node, expr, vm) {
+
+    },
+    on(node, expr, vm, eventName) {
+
+    },
+    updater: {
+        textUpdater(node, value) {
+            node.textContent = value
+        }
+    }
+}
+
 class Compiler {
     constructor(el, vm) {
         this.el = this.isElementNode(el) ? el : document.querySelector(el)
@@ -19,17 +48,37 @@ class Compiler {
         [...childNodes].forEach(child => {
             // 如果是元素节点
             if (this.isElementNode(child)) {
-                // this.compileElement(child);
+                this.compileElement(child);
             } else {
                 // 文本节点
-                // this.compileText(child);
+                this.compileText(child);
             }
             //递归遍历
             if (child.childNodes && child.childNodes.length) {
-            	console.log(child.childNodes)
                 this.compile(child);
             }
         })
+    }
+
+    compileElement(node) {
+        const attributes = node.attributes;
+        [...attributes].forEach(attr => {
+            const { name, value } = attr;
+            if (this.isDirective(name)) {
+                const [, dirctive] = name.split('-') // text html model on:click
+                const [dirName, eventName] = dirctive.split(':') // text html model click 
+                compileUtil[dirName](node, value, this.vm, eventName)
+            }
+        })
+    }
+
+    compileText(node) {
+
+    }
+
+
+    isDirective(attrName) {
+        return attrName.startsWith('v-');
     }
 
     node2Fragment(el) {
