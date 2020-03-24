9ae3b5719a2a9e06485b560bf430ec1784eb4081
chenhailiang <958304849@qq.com>
2020-03-24 19:58:51 +0800

    解析指令

--- 源码学习/code-xiaomage/MVue.js
+++ 源码学习/code-xiaomage/MVue.js
@@ -1,29 +1,52 @@
 const compileUtil = {
     getVal(expr, vm) {
-        console.log(vm.$data)
-        return expr.split('.').reduce((data,currentVal)=>{
-            console.log(currentVal)
+        // console.log(vm.$data)
+        return expr.split('.').reduce((data, currentVal) => {
+            // console.log(currentVal)
             return data[currentVal]
-        },vm.$data)
+        }, vm.$data)
     },
     text(node, expr, vm) {
-        const value = this.getVal(expr, vm);// expr:text   <div v-text="person.text"></div>
-        // const value = vm.$data[expr]
+        let value;
+        if (expr.indexOf('{{') !== -1) { // 
+            value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+                return this.getVal(args[1], vm)
+            })
+        } else {
+            value = this.getVal(expr, vm); // expr:text   <div v-text="person.text"></div>
+            // const value = vm.$data[expr]
+        }
+
         this.updater.textUpdater(node, value)
     },
     html(node, expr, vm) {
-
+        const value = this.getVal(expr, vm)
+        this.updater.htmlUpdater(node, value)
     },
     model(node, expr, vm) {
-
+        const value = this.getVal(expr, vm)
+        this.updater.modelUpdater(node, value)
     },
     on(node, expr, vm, eventName) {
+        let fn = vm.$options.methods && vm.$options.methods[expr]
+        node.addEventListener(eventName, fn.bind(vm), false)
+    },
+    bind(node,expr,vm,attrName){ // v-bind:src  :src
 
     },
     updater: {
         textUpdater(node, value) {
             node.textContent = value
-        }
+        },
+        htmlUpdater(node, value) {
+            node.textContent = value
+        },
+        modelUpdater(node, value) {
+            node.value = value
+        },
+        on(node, expr, vm, eventName) {
+
+        },
     }
 }
 
@@ -40,7 +63,7 @@ class Compiler {
         // 3.追加元素到根元素
         this.el.appendChild(fragment)
     }
-
+    // 解析指令
     compile(fragments) {
         // 1.获取子节点
         const childNodes = fragments.childNodes;
@@ -60,22 +83,36 @@ class Compiler {
         })
     }
 
+    // 拆指令 指令解析
     compileElement(node) {
         const attributes = node.attributes;
         [...attributes].forEach(attr => {
             const { name, value } = attr;
-            if (this.isDirective(name)) {
+            if (this.isDirective(name)) { // 是一个指令 v-text v-html v-model v-on:click
                 const [, dirctive] = name.split('-') // text html model on:click
                 const [dirName, eventName] = dirctive.split(':') // text html model click 
                 compileUtil[dirName](node, value, this.vm, eventName)
+
+                // 删除有指令的标签上的属性
+                node.removeAttribute('v-' + dirctive)
+            } else if (this.isEventName(name)) { // @click='handleClick'
+                let [,eventName] = name.split('@')
+                compileUtil['on'](node,value,this.vm,eventName)
             }
         })
     }
 
     compileText(node) {
-
+        // {{}} v-text
+        const content = node.textContent;
+        if (/\{\{(.+?)\}\}/.test(content)) {
+            compileUtil.text(node, content, this.vm)
+        }
     }
 
+    isEventName(attrName) {
+        return attrName.startsWith('@');
+    }
 
     isDirective(attrName) {
         return attrName.startsWith('v-');
